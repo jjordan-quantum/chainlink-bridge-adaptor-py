558621a756de611a0b89e5994fb3157061bb1aa8
jjordan-quantum <constant.space@gmail.com>
2021-08-16 22:59:46 -0700

    used request object to extract json data

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 @app.route("/test_bridge", methods=["POST"])
 def test_bridge():
+    data = request.json
+    if "update" in data:
+        update = data["update"]
+    else:
+        update = False
+    print(f"update field from request: {update}")
     return "Bridge response"
 
 
